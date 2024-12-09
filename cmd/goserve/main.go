bd3ddf6e67a33952f3e6549e20cb5fa2af12bf3d
ferdie bergado <ferdiebergado@gmail.com>
2024-12-09 11:25:09 +0800

    Disabled cache to reflect instant file changes

--- cmd/goserve/main.go
+++ cmd/goserve/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/ferdiebergado/goserve/pkg"
 )
 
-func main() {
-	const defaultPort = "8888"
+const defaultPort = "8888"
+const defaultAddress = "localhost"
+const defaultDir = "."
+const readTimeout = 10
+const writeTimeout = 10
+const idleTimeout = 60
 
-	address := flag.String("a", "localhost", "network address to bind to")
+func main() {
+	address := flag.String("a", defaultAddress, "network address to bind to")
 	port := flag.String("p", defaultPort, "port to listen on")
 
 	flag.Usage = func() {
@@ -28,12 +35,23 @@ func main() {
 
 	args := flag.Args()
 
-	dir := "."
+	dir := defaultDir
 
 	if len(args) > 0 {
 		dir = args[len(args)-1]
 	}
 
+	srv := http.Server{
+		Addr:         *address + ":" + *port,
+		Handler:      pkg.RequestLogger(pkg.DisableCache(http.FileServer(http.Dir(dir)))),
+		ReadTimeout:  readTimeout * time.Second,
+		WriteTimeout: writeTimeout * time.Second,
+		IdleTimeout:  idleTimeout * time.Second,
+	}
+
 	log.Printf("HTTP Server listening at %s:%s...\n", *address, *port)
-	log.Fatal(http.ListenAndServe(*address+":"+*port, pkg.RequestLogger(http.FileServer(http.Dir(dir)))))
+
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+	}
 }
--- pkg/middleware.go
+++ pkg/middleware.go
@@ -30,3 +30,13 @@ func RequestLogger(next http.Handler) http.Handler {
 		log.Printf("%s %s %s %d %s %s", r.Method, r.URL.Path, r.Proto, statusCode, http.StatusText(statusCode), duration)
 	})
 }
+
+func DisableCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", time.Unix(0, 0).Format(http.TimeFormat))
+
+		next.ServeHTTP(w, r)
+	})
+}
